39328fdeca94f82f7cfcb0b4bbcb3b790d6738aa
caroott <39764934+caroott@users.noreply.github.com>
2026-08-30 08:06:45 +0000

    Deploying to gh-pages from @ BioFSharp/BioFSharp.Mz@a873206673fc2a21efb6c30bd6f47e4bb01ac458 🚀

--- tools/BioFSharp.Mz.fsx
+++
@@ -1,4 +0,0 @@
-(**
-// can't yet format YamlFrontmatter (["title: BioFSharp.Mz"; "category: Description"; "categoryindex: 1"; "index: 1"], Some { StartLine = 2 StartColumn = 0 EndLine = 6 EndColumn = 8 }) to pynb markdown
-
-*)
